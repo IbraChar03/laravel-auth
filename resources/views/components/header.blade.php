38f72fa09537a5c0b69bf37c19b37bf848bc652e
Ibrahim <ibrachar68@gmail.com>
2023-02-09 15:28:53 +0100

    creazione tasto dashboard

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -65,17 +65,16 @@
                     <div class="dropdown-menu dropdown-menu-end"
                         aria-labelledby="navbarDropdown">
                         <a class="dropdown-item"
+                            href="{{ route('project.dashboard') }}">
+                            {{ __('Dashboard') }}
+                        </a>
+                        <a class="dropdown-item"
                             href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
-                        <a class="dropdown-item"
-                            href="{{ route('project.dashboard') }}"
-                            onclick="event.preventDefault();
-                                         ">
-                            {{ __('DashBoard') }}
-                        </a>
+
 
                         <form id="logout-form"
                             action="{{ route('logout') }}"
